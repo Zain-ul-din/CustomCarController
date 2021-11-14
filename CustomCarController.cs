2456e0ecb35f8a7062f25bd571d6546af983128e
Zain Ul Din <zain.personal47@gmail.com>
2021-11-14 18:14:59 +0500

    day 2

--- CustomCarController.cs
+++ CustomCarController.cs
@@ -18,14 +18,19 @@ namespace CustomControls{
         [SerializeField] private Transform[] wheelMeshs;
         [SerializeField] private WheelCollider[] wheelColliders;
         [SerializeField] private Transform com;
-        
-        [SerializeField] private float enginePower;
-        [SerializeField] private float brakeTorque;
-        [SerializeField] private float maxSteerAngle;
 
         public Rigidbody carRb { get; private set; }
         public bool canMove { get; set; }
+
+        // Car Engine Var
+        [SerializeField] private float enginePower = 3400;
+        [SerializeField] private float brakeTorque = 3000;
+        [SerializeField] private float maxSteerAngle = 30;
+
+        // Car Physics Var
+        [SerializeField] private float downForce = 50;
         
+
         public event EventHandler<Collision> OnCarCollide;
         public event EventHandler<Collider> OnCarTrigger;
         public static CustomCarController Instance { get; private set; } // active Car Instance
@@ -43,13 +48,22 @@ namespace CustomControls{
         private void Update () =>
         AlignWheels();
 
+        private void FixedUpdate () =>
+        ManageCarPhysics();
+
+        private void OnCollisionEnter (Collision collision) =>
+        OnCarCollide?.Invoke(this, collision);
+
+        private void OnTriggerEnter (Collider other) =>
+        OnCarTrigger?.Invoke(this, other);
+        
         #endregion
 
         #region Custom Func
 
         private void SetUp(){
-
-            carRb.centerOfMass = com.position;
+            carRb = GetComponent<Rigidbody>();
+            carRb.centerOfMass = com.localPosition;
 
             // setting Parent to null so, wheels can move freely
             foreach (Transform t in wheelMeshs)
@@ -67,41 +81,61 @@ namespace CustomControls{
                }
         }
 
+        internal bool isBraking;
+
         // MoveCar
         public void MoveCar(float accl,float steer,float brake){
             
-            accl = Mathf.Clamp(accl, -1, 1);
-            steer = Mathf.Clamp(steer, -maxSteerAngle, maxSteerAngle);
-            brake = Mathf.Clamp(brake, -1, 1);
+            accl = Mathf.Clamp(accl, -1, 1) * enginePower;
+            steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
+            brake = Mathf.Clamp(brake, 0, 1) * brakeTorque;
+
+            isBraking = (brake > 0) ? true : false;
 
             if (canMove){
-                Engine(accl, steer, brake);
+                if (!isBraking){
+                   Engine(accl, steer, brake);
+                }
+                else{
+                    Brake();
+                }
             }
-            else{
+            else {
                 KillEngine();
             }
+            
         }
 
         
 
         internal void Engine(float accl,float steer,float brake){
-            
+            foreach(WheelCollider wc in wheelColliders) {
+                wc.motorTorque = accl;
+                wc.brakeTorque = 0;
+            }
+
+            for(int i = 0; i < wheelColliders.Length - 2; i++){
+                wheelColliders[i].steerAngle = steer;
+            }
         }
 
         internal void KillEngine(){
-            carRb.angularDrag = 0.3f;
             foreach(WheelCollider wc in wheelColliders){
                 wc.brakeTorque = brakeTorque/wheelColliders.Length;
             }
         }
+
+        internal void Brake(){
+            foreach(WheelCollider wc in wheelColliders){
+                wc.brakeTorque = brakeTorque;
+            }
+        }
+
+        private void ManageCarPhysics(){
+            carRb.AddForce(-Vector3.up * downForce * carRb.velocity.magnitude);
+        }
         #endregion
 
-        private void OnCollisionEnter(Collision collision){
-            OnCarCollide?.Invoke(this, collision);
-        }
-
-        private void OnTriggerEnter(Collider other){
-            OnCarTrigger?.Invoke(this, other);
-        }
+        
     }
 }// namespace
---
+++ TempInputHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomControls;
+
+public class TempInputHelper : MonoBehaviour
+{
+    private CustomCarController controller;
+
+
+    // Start is called before the first frame update
+    private void OnEnable() =>
+                              controller = CustomCarController.Instance;
+
+
+    internal float x;
+    internal float y;
+    internal float brake;
+
+    // Update is called once per frame
+    void Update(){
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+            brake = 1;
+        else brake = 0;
+
+        controller.MoveCar(y, x, brake);
+    }
+
+
+}
